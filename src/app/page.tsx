edb19eacdc33aa04ab9bc33b2deb700c2bf3338e
Abdullah Aly <abdullahaly.dev@gmail.com>
2025-08-19 19:39:37 +0300

    fix: rename Home component to Page for consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Room } from "@/components/room/room-controller";
 import { SessionDetails } from "@/components/room/room-details";
 
-export default function Home() {
+export default function Page() {
   return (
     <div className="overflow-hidden cursor-none h-screen w-screen">
       <Room room="GLOBAL" />
