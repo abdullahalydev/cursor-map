b6899aa5f9d9347c75c002334cf81cfecb32135c
Abdullah Aly <abdullahaly.dev@gmail.com>
2025-08-19 19:39:24 +0300

    feat: add dynamic RoomPage component to handle room-specific rendering

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Room } from "@/components/room/room-controller";
+import { SessionDetails } from "@/components/room/room-details";
+
+export default function Home() {
+  return (
+    <div className="overflow-hidden cursor-none h-screen w-screen">
+      <Room room="GLOBAL" />
+
+      <SessionDetails />
+    </div>
+  );
+}
---
+++ src/app/room/[handle]/page.tsx
@@ -0,0 +1,18 @@
+import { Room } from "@/components/room/room-controller";
+import { SessionDetails } from "@/components/room/room-details";
+
+export type RoomPage = {
+  params: Promise<{ handle: string }>;
+};
+
+export default async function Home({ params }: RoomPage) {
+  const { handle } = await params;
+
+  return (
+    <div className="overflow-hidden cursor-none h-screen w-screen">
+      <Room room={handle} />
+
+      <SessionDetails />
+    </div>
+  );
+}
