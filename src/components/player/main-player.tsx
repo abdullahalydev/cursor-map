3652e45363ab9a403236c2ca072989d8f8c8d325
Abdullah Aly <abdullahaly.dev@gmail.com>
2025-08-19 20:46:06 +0300

    feat: add "You" sign upper MainPlayer

--- src/components/player/main-player.tsx
+++ src/components/player/main-player.tsx
@@ -37,6 +37,7 @@ export function MainPlayer() {
       animate={{ x: position[0], y: position[1] }}
       transition={{ type: "spring" }}
     >
+      <span className="text-xs bg-blue-200 rounded-full px-2 py-1 font-semibold text-blue-800">You</span>
       <Cursor fill={`rgb(${color.join(",")})`} />
     </motion.div>
   );
