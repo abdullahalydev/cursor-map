148191a4447d310b2ca5c399647be115427a62d8
Abdullah Aly <abdullahaly.dev@gmail.com>
2025-08-19 20:45:45 +0300

    fix: social links

--- src/components/room/room-social-links.tsx
+++ src/components/room/room-social-links.tsx
@@ -1,18 +1,23 @@
-
-"use client"
+"use client";
 import { Github, Twitter } from "lucide-react";
+import Link from "next/link";
 
 export function SocialLinks() {
   return (
     <div className="w-content bg-blue-200 text-blue-800 py-3 gap-2 px-3 h-14 flex items-center rounded-full">
-      <a
+      <Link
         href="#"
+        target="_blank"
         className="bg-blue-100 pointer-fine:cursor-none rounded-full p-2 h-full flex items-center hover:bg-blue-100/60"
       >
         <Github className="text-blue-800" size={18} />
-      </a>
+      </Link>
 
-      <a className=" bg-blue-100 rounded-full p-2 h-full flex items-center hover:bg-blue-100/60">
+      <a
+        href="#"
+        target="_blank"
+        className=" bg-blue-100 rounded-full p-2 h-full flex items-center hover:bg-blue-100/60"
+      >
         <Twitter className="text-blue-800" size={18} />
       </a>
     </div>
