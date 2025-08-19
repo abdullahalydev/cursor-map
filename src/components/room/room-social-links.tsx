2b3b57cfbc8a4ea91532bccd06cda301d83529d6
Abdullah Aly <abdullahaly.dev@gmail.com>
2025-08-19 21:04:43 +0300

    fix: href in SocialLinks

--- src/components/room/room-social-links.tsx
+++ src/components/room/room-social-links.tsx
@@ -6,20 +6,20 @@ export function SocialLinks() {
   return (
     <div className="w-content bg-blue-200 text-blue-800 py-3 gap-2 px-3 h-14 flex items-center rounded-full">
       <Link
-        href="#"
+        href="https://github.com/abdullahalydev"
         target="_blank"
         className="bg-blue-100 pointer-fine:cursor-none pointer-coarse:cursor-none rounded-full p-2 h-full flex items-center hover:bg-blue-100/60"
       >
         <Github className="text-blue-800" size={18} />
       </Link>
 
-      <a
-        href="#"
+      <Link
+        href="https://x.com/abdullahalydev"
         target="_blank"
         className=" bg-blue-100 pointer-fine:cursor-none pointer-coarse:cursor-none rounded-full p-2 h-full flex items-center hover:bg-blue-100/60"
       >
         <Twitter className="text-blue-800" size={18} />
-      </a>
+      </Link>
     </div>
   );
 }
